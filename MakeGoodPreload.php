aef86dbc7091425a248d253f11c08a43f29b4b72
Simon Dietschi <simon.dietschi@hin.ch>
2017-02-10 14:20:18 +0100

    Specific dependency file no longer needed

--- MakeGoodPreload.php
+++
@@ -1,6 +0,0 @@
-<?php
-// This is a preload script to be used with
-// the Eclipse makegood continuous integration plugin
-// see https://github.com/piece/makegood/releases
-error_reporting(E_ALL);
-$loader = require 'vendor/autoload.php';
