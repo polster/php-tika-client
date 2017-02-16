48545a5c6d0feac95b13635a2d700d663f6e3849
Simon Dietschi <simon.dietschi@hin.ch>
2017-02-16 16:14:53 +0100

    Use default print

--- src/TikaClientException.php
+++ src/TikaClientException.php
@@ -18,13 +18,4 @@ class TikaClientException extends Exception {
     public function __construct($message, $code = 0) {
         parent::__construct($message, $code);
     }
-
-    /**
-     * Prints this exception.
-     *
-     * @return string
-     */
-    public function __toString() {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-    }
 }
